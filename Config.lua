ae0ed1a929df13f45e396197cee8821d358cd61e
Raysam74 <leraysam74@gmail.com>
2026-01-18 15:18:43 +0100

    Add initial configuration settings in Config.lua

---
+++ Config.lua
@@ -0,0 +1,13 @@
+--test
+
+local Config = {}
+
+Config.DEFAULT_WALKSPEED = 16
+Config.SPEEDCOIL_WALKSPEED = 24
+Config.SPEED_TOLERANCE = 1.5
+Config.FLY_HEIGHT_LIMIT = 8
+Config.CHECK_INTERVAL = 0.4
+Config.MAX_WARNINGS = 3
+Config.SLIDE_ZONE_NAME = "SlideZone"
+
+return Config
